fcf8146e68d4c29a6609a6b3e7248121950315b7
jz903 <jasonzhou903@qq.com>
2024-06-12 11:15:30 +0800

    Optimize code

--- middleware.ts
+++ middleware.ts
@@ -5,13 +5,14 @@ export async function middleware(request: NextRequest) {
   const session = await getSession();
 
   if (session && !request.nextUrl.pathname.startsWith("/dashboard")) {
-    await updateSession(request);
     return Response.redirect(new URL("/dashboard", request.url));
   }
 
   if (!session && !request.nextUrl.pathname.startsWith("/login")) {
     return Response.redirect(new URL("/login", request.url));
   }
+
+  return await updateSession(request);
 }
 
 export const config = {
